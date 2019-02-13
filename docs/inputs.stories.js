540d520dcd4c236559ab6867650b120310a83c08
john5565 <40635479+john5565@users.noreply.github.com>
2019-02-13 17:28:33 +0100

    Add files via upload

---
+++ docs/inputs.stories.js
@@ -0,0 +1,40 @@
+import { storiesOf } from '@storybook/html'; // eslint-disable-line import/no-extraneous-dependencies
+import { // eslint-disable-line import/no-extraneous-dependencies
+  withKnobs, radios,
+} from '@storybook/addon-knobs';
+
+const stories = storiesOf('Inputs', module);
+stories.addDecorator(withKnobs);
+
+stories.add('input.radio', () => `
+      <label>
+        <input type="radio" class="nes-radio" name="answer" checked />
+        <span>Yes</span>
+      </label> <label>
+        <input type="radio" class="nes-radio" name="answer" />
+        <span>No</span>
+      </label>`)
+  .add('input.checkbox', () => {
+    const selectedClass = radios('class', {
+      default: '',
+      'is-dark': 'is-dark',
+    }, '');
+
+    return (
+      `<label>
+        <input type="checkbox" class="nes-checkbox ${selectedClass}" checked />
+        <span>Enable</span>
+      </label>`
+    );
+  })
+  .add('input', () => {
+    const selectedClass = radios('class', {
+      default: '',
+      'is-success': 'is-success',
+      'is-warning': 'is-warning',
+      'is-error': 'is-error',
+      'is-dark': 'is-dark',
+    }, '');
+
+    return `<input type="text" id="name_field" class="nes-input ${selectedClass}" placeholder="NES.css">`;
+  });
